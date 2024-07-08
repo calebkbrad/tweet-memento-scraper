259f1be817f10b158a51e225316505244c92b622
caleb <cbrad022@odu.edu>
2024-07-08 15:03:11 -0400

    refactor memento datetime extraction

--- src/tools.py
+++ src/tools.py
@@ -1,11 +1,20 @@
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-def get_memento_datetime(memento_datetime_header: str) -> datetime:
+def get_memento_datetime(response_headers: dict) -> datetime:
     """
     Extract appropriate datetime from Memento datetime header
+
+    Parameters
+    ----------
+    response_headers: The response headers from a request to the Wayback Machine
+
+    Returns
+    ----------
+    datetime object corresponding to the "memento-datetime" header in the response_headers
     """
 
+    memento_datetime_header = response_headers['memento-datetime']
     return datetime.strptime(memento_datetime_header, "%a, %d %b %Y %H:%M:%S GMT")
 
 def get_nov_2008_info(content: BeautifulSoup) -> dict:
