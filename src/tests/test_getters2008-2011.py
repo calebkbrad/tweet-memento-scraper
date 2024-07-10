e48da3f1c4475298788d4f76a5ece2d9eeef6b89
caleb <cbrad022@odu.edu>
2024-07-10 16:52:58 -0400

    move 2012 onwards to a separate script

--- src/tests/test_getters2008-2011.py
+++ src/tests/test_getters2008-2011.py
@@ -60,12 +60,6 @@ def apr_2011_memento_20():
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
-@pytest.fixture
-def jun_2012_memento_20():
-    url = "http://web.archive.org/web/20120608132840/https://twitter.com/jack/status/20"
-    response = requests.get(url)
-    return BeautifulSoup(response.content, 'html.parser')
-
 def test_nov_2008_20(tweet_id_20, nov_2008_memento_20):
     extracted_contents = getters2008_2011.get_nov_2008_info(nov_2008_memento_20)
     assert extracted_contents == tweet_id_20
@@ -89,8 +83,3 @@ def test_jul_2010_20(tweet_id_20, jul_2010_memento_20):
 def test_apr_2011_20(tweet_id_20, apr_2011_memento_20):
     extracted_contents = getters2008_2011.get_dec_2008_info(apr_2011_memento_20)
     assert extracted_contents == tweet_id_20
-
-def test_jun_2012_20(tweet_id_20, jun_2012_memento_20):
-    extracted_contents = getters2008_2011.get_dec_2008_info(jun_2012_memento_20)
-    assert extracted_contents == tweet_id_20
-
---
+++ src/tests/test_getters2012.py
@@ -0,0 +1,25 @@
+from getters import getters2008_2011
+import datetime
+import pytest
+import requests
+from bs4 import BeautifulSoup
+
+# Fixtures representing what we expect from our test Tweet IDs
+@pytest.fixture
+def tweet_id_20():
+    return {
+        'tweet-text': "just setting up my twttr",
+        'full-name': "Jack Dorsey",
+        'handle': "jack",
+        'date': datetime.datetime(2006, 3, 21, 12, 50)
+    }
+
+@pytest.fixture
+def jun_2012_memento_20():
+    url = "http://web.archive.org/web/20120608132840/https://twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+def test_jun_2012_20(tweet_id_20, jun_2012_memento_20):
+    extracted_contents = getters2008_2011.get_dec_2008_info(jun_2012_memento_20)
+    assert extracted_contents == tweet_id_20
