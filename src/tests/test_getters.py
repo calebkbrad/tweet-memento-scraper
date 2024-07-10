49e9410bdbb52f1a250a9bd6476bb4058d4d3144
caleb <cbrad022@odu.edu>
2024-07-10 14:58:08 -0400

    add test for sept 2009

--- src/tests/test_getters.py
+++ src/tests/test_getters.py
@@ -16,7 +16,7 @@ def tweet_id_20():
 @pytest.fixture
 def tweet_id_968105771():
     return {
-        'tweet-text': "Don't make something unless it is both necessary and useful; but if it is both, don't hesitate to make it beautiful.",
+        'tweet-text': '"Don\'t make something unless it is both necessary and useful; but if it is both, don\'t hesitate to make it beautiful."',
         'full-name': "Jack Dorsey",
         'handle': "jack",
         'date': datetime.datetime(2008, 10, 20, 15, 27)
@@ -47,3 +47,9 @@ def test_nov_2008_20(tweet_id_20, nov_2008_memento_20):
 def test_dec_2008_20(tweet_id_20, dec_2008_memento_20):
     extracted_contents = getters2008.get_dec_2008_info(dec_2008_memento_20)
     assert extracted_contents == tweet_id_20
+
+def test_sep_2009_968105771(tweet_id_968105771, sep_2009_memento_968105771):
+    extracted_contents = getters2008.get_dec_2008_info(sep_2009_memento_968105771)
+    assert extracted_contents == tweet_id_968105771
+
+
