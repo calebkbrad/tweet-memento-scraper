5a2455ce64d4536f5061843538a4021ec6b8fa55
caleb <cbrad022@odu.edu>
2024-07-12 10:37:49 -0400

    add more 2012 tests

--- src/tests/test_getters2012.py
+++ src/tests/test_getters2012.py
@@ -15,10 +15,32 @@ def tweet_id_20():
     }
 
 @pytest.fixture
+def jan_2012_memento_20():
+    url = "http://web.archive.org/web/20120112171012/https://twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def march_2012_memento_20():
+    url = "http://web.archive.org/web/20120322170404/https://twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
 def jun_2012_memento_20():
     url = "http://web.archive.org/web/20120608132840/https://twitter.com/jack/status/20"
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
+
+
+def test_jan_2012_20(tweet_id_20, jan_2012_memento_20):
+    extracted_contents = getters2008_2011.get_dec_2008_info(jan_2012_memento_20)
+    assert extracted_contents == tweet_id_20
+
+def test_march_2012_20(tweet_id_20, march_2012_memento_20):
+    extracted_contents = getters2008_2011.get_dec_2008_info(march_2012_memento_20)
+    assert extracted_contents == tweet_id_20
+
 
 def test_jun_2012_20(tweet_id_20, jun_2012_memento_20):
     extracted_contents = getters2012.get_jun_2012(jun_2012_memento_20)
