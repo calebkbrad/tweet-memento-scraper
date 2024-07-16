17ef0c1f263586fff52bc77fe716922f3474a39d
caleb <cbrad022@odu.edu>
2024-07-16 16:12:07 -0400

    tests for august 2014, do not pass yet

--- src/tests/test_getters2014.py
+++ src/tests/test_getters2014.py
@@ -40,17 +40,20 @@ def tweet_id_415211454720004096_german_withpic():
         'date': datetime.datetime(2013, 12, 23)
     }
 
+@pytest.fixture
+def tweet_id_334364760252489728():
+    return {
+        'tweet-text': "Square is now processing $15 billion annualized. That's without Starbucks included. I couldn't be more proud of our team!",
+        'full-name': "Jack Dorsey",
+        'handle': "jack",
+        'date': datetime.datetime(2013, 5, 14)
+    }
+
 
 
 @pytest.fixture
 def jan_2014_memento_334653979390779392():
     url = "https://web.archive.org/web/20140117171911/twitter.com/jack/status/334653979390779392"
-    response = requests.get(url)
-    return BeautifulSoup(response.content, 'html.parser')
-
-@pytest.fixture
-def feb_2014_memento_20():
-    url = "http://web.archive.org/web/20140209151232/twitter.com/jack/status/20"
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
@@ -66,6 +69,23 @@ def jan_2014_memento_415211454720004096_german_withpic():
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
+@pytest.fixture
+def feb_2014_memento_20():
+    url = "http://web.archive.org/web/20140209151232/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def apr_2014_memento_334364760252489728():
+    url = "http://web.archive.org/web/20140407201626/twitter.com/jack/status/334364760252489728"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def aug_2014_memento_20():
+    url = "http://web.archive.org/web/20150812205308/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
 
 
 
@@ -77,11 +97,20 @@ def test_jan_2014_398482069279748096(tweet_id_398482069279748096, jan_2014_memen
     extracted_contents = getters2012.get_jun_2012(jan_2014_memento_398482069279748096)
     assert extracted_contents == tweet_id_398482069279748096
 
+@pytest.mark.skip(reason="Date is in another language, don't currently know how to deal with this")
 def test_jan_2014_415211454720004096_german_withpic(tweet_id_415211454720004096_german_withpic, jan_2014_memento_415211454720004096_german_withpic):
     extracted_contents = getters2012.get_jun_2012(jan_2014_memento_415211454720004096_german_withpic)
     assert extracted_contents == tweet_id_415211454720004096_german_withpic
 
 def test_feb_2014_20(tweet_id_20, feb_2014_memento_20):
     extracted_contents = getters2012.get_jun_2012(feb_2014_memento_20)
+    assert extracted_contents == tweet_id_20
+
+def test_apr_2014_334364760252489728(tweet_id_334364760252489728, apr_2014_memento_334364760252489728):
+    extracted_contents = getters2012.get_jun_2012(apr_2014_memento_334364760252489728)
+    assert extracted_contents == tweet_id_334364760252489728
+
+def test_aug_2014_20(tweet_id_20, aug_2014_memento_20):
+    extracted_contents = getters2012.get_jun_2012(aug_2014_memento_20)
     assert extracted_contents == tweet_id_20
 
