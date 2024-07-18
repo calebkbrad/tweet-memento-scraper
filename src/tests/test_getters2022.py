ceded77b244e5344e5e98e791fcdf010e3813a8c
caleb <cbrad022@odu.edu>
2024-07-18 11:52:34 -0400

    add first test for 2022

---
+++ src/tests/test_getters2022.py
@@ -0,0 +1,25 @@
+from getters import getters2012
+import datetime
+import pytest
+import requests
+from bs4 import BeautifulSoup
+
+@pytest.fixture
+def tweet_id_20():
+    return {
+        'tweet-text': "just setting up my twttr",
+        'full-name': "jack",
+        'handle': "jack",
+        'date': datetime.datetime(2006, 3, 21)
+    }
+
+@pytest.fixture
+def jan_2022_memento_20():
+    url = "http://web.archive.org/web/20160122063429/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+
+def test_jan_2022_20(tweet_id_20, jan_2022_memento_20):
+    extracted_contents = getters2012.get_jun_2012(jan_2022_memento_20)
+    assert extracted_contents == jan_2022_memento_20
