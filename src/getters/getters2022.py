1c5490e50821331c5a08af3f88edfc1da9a00740
caleb <cbrad022@odu.edu>
2024-07-18 15:05:59 -0400

    getter for 2022

---
+++ src/getters/getters2022.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def get_jun_2022(content: BeautifulSoup) -> dict:
+    """
+    Extract all available info from a tweet with structure from around June 2012
+    
+    Parameters
+    ----------
+    content: BeautifulSoup
+        A BeautifulSoup object with the parsed content of a Tweet URI-M from around this time frame
+
+    Returns
+    ----------
+    dict
+        Dictionary containing all available information. At most, keys include the following:
+        tweet-text: The tweet body
+        full-name: Full name of the tweet author
+        handle: Twitter handle of the tweet author
+        date: datetime of the date the tweet was made
+    """
+    # print(content)
+    full_tweet_structure = content.find(attrs={"role": "region"})
+
+    info = {}
+    info['tweet-text'] = full_tweet_structure.find("div", itemprop="articleBody").get_text().strip()
+    info['full-name'] = full_tweet_structure.find("meta", itemprop="givenName")['content']
+    info['handle'] = full_tweet_structure.find("meta", itemprop="additionalName")['content']
+    timestamp = full_tweet_structure.find("meta", itemprop="datePublished")['content']
+    info['date'] = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S.%fZ").replace(second=0, minute=0, hour=0)
+
+    return info
--- src/tests/test_getters2022.py
+++ src/tests/test_getters2022.py
@@ -1,4 +1,4 @@
-from getters import getters2012
+from getters import getters2022
 import datetime
 import pytest
 import requests
@@ -8,18 +8,18 @@ from bs4 import BeautifulSoup
 def tweet_id_20():
     return {
         'tweet-text': "just setting up my twttr",
-        'full-name': "jack",
+        'full-name': "jack⚡️",
         'handle': "jack",
         'date': datetime.datetime(2006, 3, 21)
     }
 
 @pytest.fixture
 def jan_2022_memento_20():
-    url = "http://web.archive.org/web/20160122063429/twitter.com/jack/status/20"
+    url = "http://web.archive.org/web/20220123031752/twitter.com/jack/status/20"
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
 
 def test_jan_2022_20(tweet_id_20, jan_2022_memento_20):
-    extracted_contents = getters2012.get_jun_2012(jan_2022_memento_20)
-    assert extracted_contents == jan_2022_memento_20
+    extracted_contents = getters2022.get_jun_2022(jan_2022_memento_20)
+    assert extracted_contents == tweet_id_20
