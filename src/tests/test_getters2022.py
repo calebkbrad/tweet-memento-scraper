f122f8150f1166554d1c9649caf72b6032af10f7
caleb <cbrad022@odu.edu>
2024-07-19 12:42:06 -0400

    more test coverage for 2022

--- src/tests/test_getters2022.py
+++ src/tests/test_getters2022.py
@@ -14,8 +14,36 @@ def tweet_id_20():
     }
 
 @pytest.fixture
+def tweet_id_20_second_half():
+    return {
+        'tweet-text': "just setting up my twttr",
+        'full-name': "jack",
+        'handle': "jack",
+        'date': datetime.datetime(2006, 3, 21)
+    }
+
+
+@pytest.fixture
 def jan_2022_memento_20():
     url = "http://web.archive.org/web/20220123031752/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def may_2022_memento_20():
+    url = "http://web.archive.org/web/20220506122041/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def aug_2022_memento_20():
+    url = "http://web.archive.org/web/20220806082835/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def dec_2022_memento_20():
+    url = "http://web.archive.org/web/20221220171620/twitter.com/jack/status/20"
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
@@ -23,3 +51,15 @@ def jan_2022_memento_20():
 def test_jan_2022_20(tweet_id_20, jan_2022_memento_20):
     extracted_contents = getters2022.get_jun_2022(jan_2022_memento_20)
     assert extracted_contents == tweet_id_20
+
+def test_may_2022_20(tweet_id_20, may_2022_memento_20):
+    extracted_contents = getters2022.get_jun_2022(may_2022_memento_20)
+    assert extracted_contents == tweet_id_20
+
+def test_aug_2022_20(tweet_id_20_second_half, aug_2022_memento_20):
+    extracted_contents = getters2022.get_jun_2022(aug_2022_memento_20)
+    assert extracted_contents == tweet_id_20_second_half
+
+def test_dec_2022_20(tweet_id_20_second_half, dec_2022_memento_20):
+    extracted_contents = getters2022.get_jun_2022(dec_2022_memento_20)
+    assert extracted_contents == tweet_id_20_second_half
