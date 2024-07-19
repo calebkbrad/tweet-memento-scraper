3ce3df1a81117990c095119eb2b83997b0563a2c
caleb <cbrad022@odu.edu>
2024-07-19 13:21:32 -0400

    add 2023 tests

--- src/tests/test_getters2022-2023.py
+++ src/tests/test_getters2022-2023.py
@@ -47,6 +47,30 @@ def dec_2022_memento_20():
     response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
+@pytest.fixture
+def mar_2023_memento_20():
+    url = "http://web.archive.org/web/20230307130951/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def jul_2023_memento_20():
+    url = "http://web.archive.org/web/20230615112336/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def aug_2023_memento_20():
+    url = "http://web.archive.org/web/20230816204033/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
+@pytest.fixture
+def sept_2023_memento_20():
+    url = "http://web.archive.org/web/20230927120154/twitter.com/jack/status/20"
+    response = requests.get(url)
+    return BeautifulSoup(response.content, 'html.parser')
+
 
 def test_jan_2022_20(tweet_id_20, jan_2022_memento_20):
     extracted_contents = getters2022.get_jun_2022(jan_2022_memento_20)
@@ -62,4 +86,20 @@ def test_aug_2022_20(tweet_id_20_second_half, aug_2022_memento_20):
 
 def test_dec_2022_20(tweet_id_20_second_half, dec_2022_memento_20):
     extracted_contents = getters2022.get_jun_2022(dec_2022_memento_20)
+    assert extracted_contents == tweet_id_20_second_half
+
+def test_mar_2023_20(tweet_id_20_second_half, mar_2023_memento_20):
+    extracted_contents = getters2022.get_jun_2022(mar_2023_memento_20)
+    assert extracted_contents == tweet_id_20_second_half
+
+def test_jul_2023_20(tweet_id_20_second_half, jul_2023_memento_20):
+    extracted_contents = getters2022.get_jun_2022(jul_2023_memento_20)
+    assert extracted_contents == tweet_id_20_second_half
+
+def test_aug_2023_20(tweet_id_20_second_half, aug_2023_memento_20):
+    extracted_contents = getters2022.get_jun_2022(aug_2023_memento_20)
+    assert extracted_contents == tweet_id_20_second_half
+
+def test_sept_2023_20(tweet_id_20_second_half, sept_2023_memento_20):
+    extracted_contents = getters2022.get_jun_2022(sept_2023_memento_20)
     assert extracted_contents == tweet_id_20_second_half
