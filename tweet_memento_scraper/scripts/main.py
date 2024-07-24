560a07a6f874894dc1759b4963230fc270610cb2
caleb <cbrad022@odu.edu>
2024-07-24 15:05:24 -0400

    setup skeleton for command to scrape one tweet

--- tweet_memento_scraper/scripts/main.py
+++ tweet_memento_scraper/scripts/main.py
@@ -1,4 +1,9 @@
 import click
+import bs4
+import requests
+from . import tools
+from .. import getters
+
 
 @click.command()
 def cli():
@@ -6,3 +11,18 @@ def cli():
     Click test
     """
     click.echo("This is just a test")
+
+def scrape_tweet(uri: str) -> dict:
+    """
+    Scrape the tweet for available info, return as dict
+    """
+    
+    
+
+@click.command()
+@click.argument("uri")
+def scrape_tweet_memento(uri: str):
+    """
+    Scrape the URI-R given by URI argument
+    """
+    print(uri)
