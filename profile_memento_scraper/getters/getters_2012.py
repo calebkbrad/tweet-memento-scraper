423057f8e5b37f6b0f27259985a8aca3eba0f2df
caleb <cbrad022@odu.edu>
2024-10-17 12:28:13 -0400

    fix ambiguous structures during this time period

--- profile_memento_scraper/getters/getters_2012.py
+++ profile_memento_scraper/getters/getters_2012.py
@@ -17,21 +17,34 @@ def get_profile_may_2012(content: BeautifulSoup) -> dict:
         full-name: The current screen name of the user
     """
     info = {}
-
-    info['handle'] = content.find("div", "screen-name").text.strip()
-    info['full-name'] = content.find("span", "fn").text.strip()
-
     tweets = []
-    tweet_list = content.find("ol", "statuses").find_all("li")
-    for tweet in tweet_list:
-        author = tweet.find("a", "screen-name")
-        if author and author.text.strip() != info['handle']:
-            continue
-        tweet_info = {}
-        tweet_info['text'] = tweet.find("span", "entry-content").text.strip()
-        tweet_info['date'] = tweet.find("span", "timestamp")['data']
-        tweets.append(tweet_info)
+    # This time has differing structures, so need different scrapers
+    if content.find("div", "screen-name"):
+        info['handle'] = content.find("div", "screen-name").text.strip()
+        info['full-name'] = content.find("span", "fn").text.strip()
 
+        tweet_list = content.find("ol", "statuses").find_all("li")
+        for tweet in tweet_list:
+            author = tweet.find("a", "screen-name")
+            if author and author.text.strip() != info['handle']:
+                continue
+            tweet_info = {}
+            tweet_info['text'] = tweet.find("span", "entry-content").text.strip()
+            tweet_info['date'] = tweet.find("span", "timestamp")['data']
+            tweets.append(tweet_info)
+    elif content.find("span", "screen-name"):
+        info['handle'] = content.find("span", "screen-name").text.strip().split("@")[1]
+        info['full-name'] = content.find("h1", "fullname").text.strip()
+
+        tweet_list = content.find_all("div", "content")
+        for tweet in tweet_list:
+            author = tweet.find("strong", "fullname")
+            if author and author.text.strip() != info['full-name']:
+                continue
+            tweet_info = {}
+            tweet_info['text'] = tweet.find("p", "js-tweet-text").text.strip()
+            tweet_info['date'] = tweet.find("a", "tweet-timestamp")['title']
+            tweets.append(tweet_info)
     info['tweets'] = tweets
 
     return info
